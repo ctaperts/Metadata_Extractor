a338ef5c99f877737a8f3a358e828be71a531520
Colby Taperts <colbytaperts@gmail.com>
2016-07-13 08:54:13 -0500

    fix issue in Metadata_finder.rb

--- Metadata_finder.rb
+++ Metadata_finder.rb
@@ -1,11 +1,11 @@
 
 puts "Text metadata extractor - by Colby Taperts colbytaperts@gmail.com\n"
 
-#puts "Look at all of the text documents before adding input"
-#puts "What kind of search would you like? choose the corresponding option below"
+puts "Look at all of the text documents before adding input"
+puts "What kind of search would you like? choose the corresponding option below"
 #options
 
-#puts "Add a word to search on and extract for example SENT:"
+puts "Add a word to search on and extract for example SENT:"
 #custom search
 
 txt_files = Dir["./**/*.txt"]
@@ -13,6 +13,14 @@ txt_files = Dir["./**/*.txt"]
 find_text = [['\A', 'FROM=', 'Regexp::IGNORECASE'], ['\A', 'SENT:', ''], ['\A', 'TO:', ''], ['\A', 'SUBJECT:', '']]
 
 meta_out = []
+
+meta_out << " "	
+
+find_text.each do |pat, word, mod|
+	meta_out << word
+end
+
+meta_out << "\n"	
 
 txt_files.each do |file_name|
 	if !File.directory? file_name
@@ -24,12 +32,12 @@ txt_files.each do |file_name|
 				file.each_line do |line|
 					if line =~ Regexp.new("#{pat}#{word}", "#{mod}")
 						#puts "Found: #{line}"
-						meta_out << "#{line.chomp}"	
+						meta_out << line.chomp
 					end
 				end
 			end
 		end
-		end
+	end
 end
 meta_out
 meta_out_final = "" + meta_out.join("^^") + ""
@@ -37,7 +45,22 @@ meta_out_final.gsub! /(,)/, ""
 meta_out_final.gsub! /(\^\^)/, ","
 meta_out_final.gsub! /^(,)/, ""
 
-#puts meta_out_final
-open('output.csv', 'w') { |f|
+puts meta_out_final
+open('myfile.csv', 'w') { |f|
   f.puts "#{meta_out_final}"
 }
+
+
+#find1 = contents.lines.grep(/FROM/i).grep_v(/ from /)
+#find2 = contents.lines.grep(/TO:/i).grep_v(/mailto/)
+#find3 = contents.lines.grep(/CC:/i)
+#find4 = contents.lines.grep(/SUBJECT:/i)
+#find5 = contents.lines.grep(/DATE RECEIVED:/i)
+#puts input_filename
+#puts find1
+#puts find2
+#puts find3
+#puts find4
+#puts find5
+
+#puts txt_files

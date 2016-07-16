99e1ca92fb0338bd151648324cb64cccb7970652
Colby Taperts <colbytaperts@gmail.com>
2016-07-16 13:48:17 -0500

    reworked meta extractor to extract and add empty lines for nothing

--- Metadata_finder.rb
+++ Metadata_finder.rb
@@ -1,15 +1,16 @@
 
-puts "Text metadata extractor - by Colby Taperts colbytaperts@gmail.com\n"
-
-puts "Look at all of the text documents before adding input"
-puts "What kind of search would you like? choose the corresponding option below"
+#puts "Text metadata extractor - by Colby Taperts colbytaperts@gmail.com\n"
+#
+#puts "Look at all of the text documents before adding input"
+#puts "What kind of search would you like? choose the corresponding option below"
 #options
 
-puts "Add a word to search on and extract for example SENT:"
+#puts "Add a word to search on and extract for example SENT:"
 #custom search
 
 txt_files = Dir["./**/*.txt"]
 
+#find_text = {'FROM=' => ['\A', 'Regexp::IGNORECASE'], 'SENT:' => ['\A', ''], 'TO:' => ['\A', ''], 'SUJECT:' => ['\A', '']}
 find_text = [['\A', 'FROM=', 'Regexp::IGNORECASE'], ['\A', 'SENT:', ''], ['\A', 'TO:', ''], ['\A', 'SUBJECT:', '']]
 
 meta_out = []
@@ -20,25 +21,50 @@ find_text.each do |pat, word, mod|
 	meta_out << word
 end
 
-meta_out << "\n"	
+
+#puts txt_files
+
+
 
 txt_files.each do |file_name|
-	if !File.directory? file_name
-	meta_out << "\n"
-		#puts file_name
-		meta_out << "#{file_name.chomp}"
-		find_text.each do |pat, word, mod|
-			File.open(file_name) do |file|
-				file.each_line do |line|
-					if line =~ Regexp.new("#{pat}#{word}", "#{mod}")
-						#puts "Found: #{line}"
-						meta_out << line.chomp
-					end
-				end
-			end
-		end
-	end
+meta_out << "\n"	
+puts file_name
+meta_out << file_name
+    find_text.each do |pat, word, mod|
+        if !File.directory? file_name
+        open_file = File.open("#{file_name}")
+        read_file = open_file.read
+            if read_file.lines.grep(/#{word}/i).size > 0
+                puts read_file.lines.grep(/#{word}/)
+                meta_out << read_file.lines.grep(/#{word}/)
+            else
+                puts word
+            end
+        end
+    end
 end
+
+#puts meta_out
+
+#find1 = contents.lines.grep(/FROM/i).grep_v(/ from /)
+
+#txt_files.each do |file_name|
+	#if !File.directory? file_name
+	#meta_out << "\n"
+		##puts file_name
+		#meta_out << "#{file_name.chomp}"
+		#find_text.each do |pat, word, mod|
+			#File.open(file_name) do |file|
+				#file.each_line do |line|
+					#if line =~ Regexp.new("#{pat}#{word}", "#{mod}")
+						##puts "Found: #{line}"
+						#meta_out << line.chomp
+					#end
+				#end
+			#end
+		#end
+	#end
+#end
 meta_out_final = "" + meta_out.join("^^") + ""
 meta_out_final.gsub!(/(,)/, "")
 meta_out_final.gsub!(/(\^\^)/, ",")
